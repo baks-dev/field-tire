e420f50f92451c1faad79dd8b89f1ce7120e23a8
kingstar <admin@baks.dev>
2024-06-01 11:23:09 +0300

    Baks Development

--- Homologation/Resources/translations/field.tire.homologation.ru.php
+++ Homologation/Resources/translations/field.tire.homologation.ru.php
@@ -1,13 +1,155 @@
 <?php
 
 return [
-	'placeholder' => 'Выберите сезонность из списка ...',
-	'label' => 'Сезонность',
+    'placeholder' => 'Выберите Омологацию из списка ...', // Homologation
+    'label' => 'Омологация',
 
-	'summer'=> 'Летние',
-	'winter' => 'Зимние',
-	'all' => 'Всесезонные',
-	'doesnt' => 'Не важно',
+    'false' => 'Отсутствует',
+    'Iflake' => 'I*',
 
-	'studs' => 'Шипы'
+    'snowflake' => '*',
+
+    'ALP' => ' (Alpina)',
+    'AR' => ' (Alfa Romeo)',
+    'ARR' => ' (Alfa Romeo)',
+    'AM' => ' (Aston Martin)',
+    'AM2' => ' (Aston Martin)',
+    'AM4' => ' (Aston Martin)',
+    'A4A' => ' (Aston Martin)',
+    'A6A' => ' (Aston Martin)',
+    'A7A' => ' (Aston Martin)',
+    'A8A' => ' (Aston Martin)',
+    'AM8' => ' (Aston Martin)',
+    'AM9' => ' (Aston Martin)',
+    'AMP' => ' (Aston Martin)',
+    'AMR' => ' (Aston Martin)',
+    'AMS' => ' (Aston Martin)',
+    'AMX' => ' (Aston Martin)',
+    'AMV' => ' (Aston Martin)',
+    'AO' => ' (Audi)',
+
+    'RO1' => ' (Audi)',
+    'RO2' => ' (Audi)',
+    'AOE' => ' (Audi Original Extended)',
+    'AO1' => ' (Audi)',
+    'АО1' => ' (Audi)',
+    'АО2' => ' (Audi)',
+    'MONO' => ' (BAC MONO)',
+    'B' => ' (Bentley)',
+    'B1' => ' (Bentley)',
+    'BC' => ' (Bentley)',
+    'BL' => ' (Bentley)',
+    '*' => ' (BMW, Mini, Rolls Royce)',
+
+    'I' => ' (NIO)',
+    'I*' => ' (BMW)', // ???????????
+    'II*' => ' (BMW, Mini, Rolls Royce)', // ???????????
+
+    'RSC' => ' (RunFlat System Component)',
+
+    'C1' => ' (Chrysler)',
+    'ME2' => ' (Dallara)',
+
+    'F' => ' (Ferrari)',
+    'F01' => ' (Ferrari)',
+    'F02' => ' (Ferrari)',
+    'F03' => ' (Ferrari)',
+    'F04' => ' (Ferrari)',
+    'K-1' => ' (Ferrari)',
+    'K-2' => ' (Ferrari)',
+    'K1' => ' (Ferrari)',
+
+    'FO' => ' (Ford)',
+    'TV' => ' (Ford)',
+
+    'TPC SPEC' => ' (Tire Performance Criteria Specification)',
+
+    'GOE' => ' (Genesis)',
+    'HN' => ' (Hyundai)',
+    'H0' => ' (Honda)',
+    'J' => ' (Jaguar)',
+    'J1' => ' (Jaguar)',
+    'JRS' => ' (Jaguar)',
+    'JAG' => ' (Jaguar)',
+    'JP' => ' (Jeep)',
+    'L' => ' (Lamborghini)',
+    'L1' => ' (Lamborghini)',
+
+    'LR' => ' (Land Rover)',
+    'E LR' => ' (Land Rover)',
+    'LR1' => ' (Land Rover)',
+    'LR2' => ' (Land Rover)',
+    'LR3' => ' (Land Rover)',
+    'LR4' => ' (Land Rover)',
+    'LR5' => ' (Land Rover)',
+
+    'LS' => ' (Lotus)',
+    'MGT' => ' (Maserati)',
+    'MGT1' => ' (Maserati)',
+
+    'MC' => ' (MCLaren)',
+    'MC1' => ' (MCLaren)',
+    'MC2' => ' (MCLaren)',
+    'MC-C' => ' (MCLaren)',
+
+    'E MO' => ' (Mercedes)',
+    'MO' => ' (Mercedes)',
+    'MO1' => ' (Mercedes)',
+    'MOE' => ' (Mercedes Original Extended)',
+    'MOE-S' => ' (Mercedes Original Extended Silent)',
+    'MO-S ' => ' (Mercedes Original Silent)',
+    'MO-V ' => ' (Mercedes Original Vans)',
+    'MO1A' => ' (Mercedes)',
+    'MO1B' => ' (Mercedes)',
+    'MO-P' => ' (Mercedes)',
+
+
+    'HP' => ' (Pagani)',
+    'N0' => ' (Porsche)',
+    'N1' => ' (Porsche)',
+    'N2' => ' (Porsche)',
+    'N3' => ' (Porsche)',
+    'N4' => ' (Porsche)',
+    'N5' => ' (Porsche)',
+    'N6' => ' (Porsche)',
+    'PO' => ' (Porsche)',
+    'NA0' => ' (Porsche 911/992)',
+    'NA1' => ' (Porsche 911/992)',
+    'NB0' => ' (Porsche Boxter/Cayman',
+    'NC0' => ' (Porsche Cayenne)',
+    'ND0' => ' (Porsche)',
+    'ND1' => ' (Porsche)',
+    'ND2' => ' (Porsche Panamera)',
+    'NE0' => ' (Porsche Macan)',
+    'NF0' => ' (Porsche Taycan)',
+    'RIV' => ' (Rivian)',
+    'T0' => ' (Tesla)',
+    'T2' => ' (Tesla)',
+    'BLE' => ' (Tesla)',
+    'VO' => ' (Volkswagen)',
+    '+' => ' (Volkswagen)',
+    'plus' => ' (Volkswagen)',
+    'VOL' => ' (Volvo)',
+
+
+    'CH' => '',
+    'FI' => '',
+
+
+    'LM1' => ' (Lucid)',
+    'MI' => '',
+    'NA5' => ' (Porsche)',
+    'OP' => ' (Opel)',
+    'PE' => ' (Peugeot)',
+    'PO1' => '',
+    'RE' => ' (Renault)',
+    'RO3' => ' (Audi RS)',
+    'SE' => ' (Seat)',
+    'SI' => ' (Citroen)',
+    'SK' => ' (Skoda)',
+    'T1' => ' (Tesla)',
+
+    'VW' => ' Volkswagen',
+
+
 ];
