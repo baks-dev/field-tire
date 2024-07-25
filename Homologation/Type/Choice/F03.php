537799dff085e7584bfa5a0406962989fb1bff11
kingstar <admin@baks.dev>
2024-07-25 10:41:01 +0300

    Baks Development

--- Homologation/Type/Choice/F03.php
+++ Homologation/Type/Choice/F03.php
@@ -32,7 +32,7 @@ use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 #[AutoconfigureTag('baks.tire.homologation')]
 final class F03 implements TireHomologationInterface
 {
-    /** Отсутствует */
+
     public const HOMOLOGATION = 'F03';
 
     /** Возвращает значение (value) */
